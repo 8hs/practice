b494444ef40551d403f0f458d21ab298af469e50
Weever <8hs@weever.net>
2014-05-14 21:20:42 -0500

    iterator

--- src/net/weever/Sort.java
+++ src/net/weever/Sort.java
@@ -2,7 +2,9 @@ package net.weever;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Sort {
 
@@ -14,6 +16,17 @@ public class Sort {
 		List<String> list = Arrays.asList(a);
 		Collections.sort(list);
 		System.out.println(list);
+		Iterator<String> t = list.iterator();
+		while(t.hasNext()){
+			System.out.println(t.next());
+		}
+		ListIterator<String> lt = list.listIterator();
+		while(lt.hasNext()){
+			lt.set(lt.next()+"#");
+		}
+		while(lt.hasPrevious()){
+			System.out.println(lt.previous());
+		}
 	}
 
 }
