38d516b5392061f3e5f1ed03dfd25210cb0c6d67
weever <gsm@live.com>
2017-06-19 00:00:24 -0700

    matrix

---
+++ src/net/weever/datastructure/Interval.java
@@ -0,0 +1,11 @@
+package net.weever.datastructure;
+
+/**
+ * Created by localadmin on 6/18/17.
+ */
+public class Interval {
+  public int start;
+  public int end;
+  public Interval() { start = 0; end = 0; }
+  public Interval(int s, int e) { start = s; end = e; }
+}
